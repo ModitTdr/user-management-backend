49d5d61652465cdd530336198f32ea4486f726b5
ModitTdr <modittuladhar@gmail.com>
2026-05-11 10:04:04 +0545

    fix: payload wrong data during refresh

--- app/api/v1/auth/refresh-token/route.ts
+++ app/api/v1/auth/refresh-token/route.ts
@@ -1,39 +1,40 @@
 import { ok, unauthorized } from "@/lib/api-response";
 import { decryptToken, generateToken, setCookies } from "@/lib/authHelper";
 import { prisma } from "@/lib/prisma";
+import { TokenPayload } from "@/types/auth";
 import { cookies } from "next/headers";
 
 export async function POST() {
   const cookieStore = await cookies();
   const oldRefreshToken = cookieStore.get("refreshToken")?.value;
-
   if (!oldRefreshToken) {
     return unauthorized("Refresh Token is missing");
   }
 
-  const payload = decryptToken(oldRefreshToken, "refresh");
+  let payload: TokenPayload;
+  try {
+    payload = decryptToken(oldRefreshToken, "refresh") as TokenPayload;
+  } catch {
+    return unauthorized("Refresh token expired or invalid");
+  }
   const user = await prisma.user.findUnique({
-    where: { username: payload.id }
+    where: { id: payload.id }
   })
 
-  if (!user) {
-    return unauthorized("Invalid token");
-  }
-  if (user.refreshToken !== oldRefreshToken) {
+  if (!user || user.refreshToken !== oldRefreshToken) {
     return unauthorized("Invalid refresh token");
   }
-
-  const accessToken = generateToken(user, 'access');
-  const refreshToken = generateToken(user, 'refresh');
-
+  const userPayload = { id: user.id, username: user.username, role: user.role };
+  const accessToken = generateToken(userPayload, 'access');
+  const refreshToken = generateToken(userPayload, 'refresh');
   await prisma.user.update({
     where: { id: user.id },
     data: {
       refreshToken: refreshToken
     }
   });
-  await setCookies("refreshToken", accessToken);
-  await setCookies("accessToken", refreshToken);
+  await setCookies("accessToken", accessToken);
+  await setCookies("refreshToken", refreshToken);
 
   return ok("New Token Generated", { accessToken });
 }
